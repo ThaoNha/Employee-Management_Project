889c5a43a2216ae8317be20867054787937b1bfa
tthaonha <tthaonha@tma.com.vn>
2022-05-05 18:03:49 +0700

    validation

--- BackEnd/src/main/java/vn/com/tma/training/ProjectTraining/dto/EmployeeDTO.java
+++ BackEnd/src/main/java/vn/com/tma/training/ProjectTraining/dto/EmployeeDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Component
@@ -11,13 +12,21 @@ import java.util.Date;
 @NoArgsConstructor
 public class EmployeeDTO {
     private int no;
+    @NotEmpty(message = "Full name is not empty")
     private String fullName;
+    @Min(value = 18, message = "Age should not be less than 18")
+    @Max(value = 70, message = "Age should not be greater than 70")
     private int age;
     private boolean isMale;
     private String address;
+    @Positive(message = "Money Per Hour must be greater than 0")
     private double moneyPerHour;
+    @Size(min = 10, max = 11, message
+            = "Phone number must be between 10 and 11 number")
     private String phone;
+    @PastOrPresent(message = " Start day must be past or present today")
     private Date startDay;
+    @Positive(message = "Team must be greater than 0 ")
     private int teamID;
 
 
