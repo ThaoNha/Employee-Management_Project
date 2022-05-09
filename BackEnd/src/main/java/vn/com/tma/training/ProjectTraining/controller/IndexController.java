bf349b2ec8d565018c95fa559f1fbb886b10f5a4
tthaonha <tthaonha@tma.com.vn>
2022-05-09 16:11:13 +0700

    update crossorigin

--- BackEnd/src/main/java/vn/com/tma/training/ProjectTraining/controller/IndexController.java
+++ BackEnd/src/main/java/vn/com/tma/training/ProjectTraining/controller/IndexController.java
@@ -1,5 +1,6 @@
 package vn.com.tma.training.ProjectTraining.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
