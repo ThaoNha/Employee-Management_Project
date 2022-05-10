8bf758998dcb244fc88aa33d08a59b3108328c1e
tthaonha <tthaonha@tma.com.vn>
2022-05-10 15:03:51 +0700

    format code

--- src/main/java/vn/com/tma/training/ProjectTraining/repository/AdvanceRepository.java
+++ src/main/java/vn/com/tma/training/ProjectTraining/repository/AdvanceRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import vn.com.tma.training.ProjectTraining.entity.AdvanceEntity;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AdvanceRepository extends CrudRepository<AdvanceEntity, Integer> {
     Iterable<AdvanceEntity> findAllByEmployeeNo(Integer employee_id);
 
-    @Query(value = "select a.* from advance a where employee_id=:id and month(a.date)=:month", nativeQuery = true)
-    List<AdvanceEntity> findAllByEmployeeNoAndMonth(@Param("id") Integer id, @Param("month") int month);
+    @Query(value = "select a.* from advance a where employee_id=:id and a.date between :startDate and :endDate", nativeQuery = true)
+    List<AdvanceEntity> findAllByEmployeeNoAndMonth(@Param("id") Integer id, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
