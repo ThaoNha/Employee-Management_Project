51cb3bc9f857ccecb512f24309a6677463076a0b
tvtri <tvtri@tma.com.vn>
2022-05-13 17:57:42 +0700

    commit

--- BackEnd/src/main/java/vn/com/tma/training/ProjectTraining/repository/deleted/EmployeeDeletedRepository.java
+++
@@ -1,7 +0,0 @@
-package vn.com.tma.training.ProjectTraining.repository.deleted;
-
-import org.springframework.data.repository.CrudRepository;
-import vn.com.tma.training.ProjectTraining.entity.entityDeleted.EmployeeDeletedEntity;
-
-public interface EmployeeDeletedRepository extends CrudRepository<EmployeeDeletedEntity, Integer> {
-}
--- BackEnd/src/main/java/vn/com/tma/training/ProjectTraining/service/StatisticService.java
+++
@@ -1,7 +0,0 @@
-package vn.com.tma.training.ProjectTraining.service;
-
-import vn.com.tma.training.ProjectTraining.response.StatisticResponse;
-
-public interface StatisticService {
-    public StatisticResponse getStatistic(Integer id);
-}
