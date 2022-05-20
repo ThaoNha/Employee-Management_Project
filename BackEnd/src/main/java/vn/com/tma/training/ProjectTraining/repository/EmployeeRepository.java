e30add875beb71adcc51fabbbcbe96a349129268
tthaonha <tthaonha@tma.com.vn>
2022-05-20 09:26:22 +0700

    update employee service

--- BackEnd/src/main/java/vn/com/tma/training/ProjectTraining/repository/EmployeeRepository.java
+++ BackEnd/src/main/java/vn/com/tma/training/ProjectTraining/repository/EmployeeRepository.java
@@ -1,11 +1,9 @@
 package vn.com.tma.training.ProjectTraining.repository;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import vn.com.tma.training.ProjectTraining.entity.EmployeeEntity;
 import vn.com.tma.training.ProjectTraining.entity.TeamEntity;
 
@@ -18,9 +16,9 @@ public interface EmployeeRepository extends CrudRepository<EmployeeEntity, Integ
 
     Page<EmployeeEntity> findAllByTeam(TeamEntity team, Pageable of);
 
-    @Query(value = "select * from Employee e", nativeQuery = true)
-    Page<EmployeeEntity> findAllWithPageIndex(Pageable pageable);
+    @Query(value = "select e.no, e.full_name, e.age, e.male, e.address, e.money_per_hour,e.phone,e.start-day from employee e,", nativeQuery = true)
+    Page<EmployeeEntity> findAll(Pageable pageable);
 
     @Query(value = "select e.money_per_hour from Employee e where e.no=:id", nativeQuery = true)
-    double findMoneyPerHourByNo( Integer id);
+    double findMoneyPerHourByNo(Integer id);
 }
